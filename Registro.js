5ffce235762cbbe3b58970be560742e33c75f980
raphaverissimo <113625364+raphaverissimo@users.noreply.github.com>
2026-07-10 08:59:10 -0400

    Update Registro.js

--- Registro.js
+++ Registro.js
@@ -1,17 +1,1 @@
 
-const mongoose = require('./database');
-
-const RegistroSchema = new mongoose.Schema({
-    _id: String,
-    hostname: String,
-    startTime: Date,
-    startTimeLocal: String,
-    cmdLine: {
-        pid: Number
-    },
-    buildinfo: Object
-});
-
-const Registro = mongoose.model('Registro', RegistroSchema);
-
-module.exports = Registro;
