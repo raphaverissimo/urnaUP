0ab73389a0183e061c2bd9fb9f3d2695aa155e1d
raphaverissimo <113625364+raphaverissimo@users.noreply.github.com>
2026-07-10 08:50:34 -0400

    Add Registro model with schema definition

--- Registro.js
+++ Registro.js
@@ -1,1 +1,17 @@
 
+const mongoose = require('./database');
+
+const RegistroSchema = new mongoose.Schema({
+    _id: String,
+    hostname: String,
+    startTime: Date,
+    startTimeLocal: String,
+    cmdLine: {
+        pid: Number
+    },
+    buildinfo: Object
+});
+
+const Registro = mongoose.model('Registro', RegistroSchema);
+
+module.exports = Registro;
